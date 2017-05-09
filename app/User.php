2f4a855da2880de1b31abc19c9412c9032881b0e
vrajesh <vrajeshdoshi@gmail.com>
2017-05-09 23:11:39 +0530

    First user registered and is able to login

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use PHPZen\LaravelRbac\Traits\Rbac;
 class User extends Authenticatable
 {
     use Notifiable;
